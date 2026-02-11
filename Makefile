d51808d6b7e6a8549d7f5059f059fff87ed8ce92
dcdjr <ddipietro8227@gmail.com>
2026-02-10 22:55:59 -0500

    Add basic shell input loop and enable GNU extensions for getline

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC = gcc
-CFLAGS = -Wall -Wextra -Werror -std=c11 -g -Iinclude
+CFLAGS = -Wall -Wextra -Werror -std=c11 -g -Iinclude -D_GNU_SOURCE
 SRC = src/main.c src/shell.c src/parser.c src/builtins.c
 OBJ = $(SRC:.c=.o)
 
--- src/shell.c
+++ src/shell.c
@@ -1,6 +1,21 @@
 #include "shell.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void shell_loop(void) {
-    printf("myshell starting...\n");
+    char *line = NULL;
+    size_t len = 0;
+
+    while (1) {
+        printf("%s", "myshell> ");
+        fflush(stdout);
+
+        if (getline(&line, &len, stdin) == -1) {
+            break;
+        }
+
+
+    }
+
+    free(line);
 }
